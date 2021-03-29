993cab75d9d835777cd7183d0cf019ec8d70561d
Junha Park <tunatone.0111@gmail.com>
2021-03-30 01:43:02 +0900

    .

--- assignment-2/src/main.rs
+++ assignment-2/src/main.rs
@@ -5,12 +5,6 @@ use std::{collections::HashMap, env, fs, io::Write, process::exit};
 use users::{get_current_uid, get_user_by_uid};
 
 fn main() {
-    let mut level: HashMap<&str, i8> = HashMap::new();
-    level.insert("UNCLASSIFIED", 0);
-    level.insert("CONFIDENTIAL", 1);
-    level.insert("SECRET", 2);
-    level.insert("TOP_SECRET", 3);
-
     let cli_args: Vec<String> = env::args().collect();
     if (cli_args.len() < 3) || (cli_args[1] != "read") {
         panic!("Invalid command! Usage: ./mac read <document file>");
@@ -21,11 +15,21 @@ fn main() {
     let user = get_user_by_uid(get_current_uid()).unwrap();
     let username = user.name().to_str().unwrap();
 
+    // get mac.policy
     let perms_raw = fs::read_to_string("mac.policy").expect("Unexpected Error.");
     let mut perms: HashMap<&str, i8> = HashMap::new();
     for perm_raw in perms_raw.lines() {
         let kv = perm_raw.split(":").collect::<Vec<&str>>();
-        perms.insert(kv[0], *level.get(kv[1]).unwrap());
+        perms.insert(
+            kv[0],
+            match kv[1].as_ref() {
+                "UNCLASSIFIED" => 0,
+                "CONFIDENTIAL" => 1,
+                "SECRET" => 2,
+                "TOP_SECRET" => 3,
+                _ => -1,
+            },
+        );
     }
 
     if !perms.contains_key(&username) {
@@ -52,9 +56,11 @@ fn main() {
         _ => String::from("ACCESS DENIED"),
     };
 
+    // drop root permission
     seteuid(getuid()).unwrap();
     setegid(getgid()).unwrap();
 
+    // logging and printing
     let mut options = OpenOptions::new();
     options.mode(0o640).append(true).create(true);
     let log_filename = format!("{}.log", username);
